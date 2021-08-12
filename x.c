4f2a3274f30ba520c8496b07c38b0bfc49b03663
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2021-08-12 22:20:47 +0200

    Not compiling

--- x.c
+++ x.c
@@ -726,6 +726,424 @@ void xloadcols(void) {
   loaded = 1;
 }
 
+void selpaste(const Arg *dummy) {
+  XConvertSelection(xw.dpy, XA_PRIMARY, xsel.xtarget, XA_PRIMARY, xw.win,
+                    CurrentTime);
+}
+
+void numlock(const Arg *dummy) { win.mode ^= MODE_NUMLOCK; }
+
+void zoom(const Arg *arg) {
+  Arg larg;
+
+  larg.f = usedfontsize + arg->f;
+  zoomabs(&larg);
+}
+
+void zoomabs(const Arg *arg) {
+  xunloadfonts();
+  xloadfonts(usedfont, arg->f);
+  cresize(0, 0);
+  redraw();
+  xhints();
+}
+
+void zoomreset(const Arg *arg) {
+  Arg larg;
+
+  if (defaultfontsize > 0) {
+    larg.f = defaultfontsize;
+    zoomabs(&larg);
+  }
+}
+
+void ttysend(const Arg *arg) { ttywrite(arg->s, strlen(arg->s), 1); }
+
+int evcol(XEvent *e) {
+  int x = e->xbutton.x - borderpx;
+  LIMIT(x, 0, win.tw - 1);
+  return x / win.cw;
+}
+
+int evrow(XEvent *e) {
+  int y = e->xbutton.y - borderpx;
+  LIMIT(y, 0, win.th - 1);
+  return y / win.ch;
+}
+
+void mousesel(XEvent *e, int done) {
+  int type, seltype = SEL_REGULAR;
+  uint state = e->xbutton.state & ~(Button1Mask | forcemousemod);
+
+  for (type = 1; type < LEN(selmasks); ++type) {
+    if (match(selmasks[type], state)) {
+      seltype = type;
+      break;
+    }
+  }
+  selextend(evcol(e), evrow(e), seltype, done);
+  if (done)
+    setsel(getsel(), e->xbutton.time);
+}
+
+void mousereport(XEvent *e) {
+  int len, x = evcol(e), y = evrow(e), button = e->xbutton.button,
+           state = e->xbutton.state;
+  char buf[40];
+  static int ox, oy;
+
+  /* from urxvt */
+  if (e->xbutton.type == MotionNotify) {
+    if (x == ox && y == oy)
+      return;
+    if (!IS_SET(MODE_MOUSEMOTION) && !IS_SET(MODE_MOUSEMANY))
+      return;
+    /* MOUSE_MOTION: no reporting if no button is pressed */
+    if (IS_SET(MODE_MOUSEMOTION) && oldbutton == 3)
+      return;
+
+    button = oldbutton + 32;
+    ox = x;
+    oy = y;
+  } else {
+    if (!IS_SET(MODE_MOUSESGR) && e->xbutton.type == ButtonRelease) {
+      button = 3;
+    } else {
+      button -= Button1;
+      if (button >= 3)
+        button += 64 - 3;
+    }
+    if (e->xbutton.type == ButtonPress) {
+      oldbutton = button;
+      ox = x;
+      oy = y;
+    } else if (e->xbutton.type == ButtonRelease) {
+      oldbutton = 3;
+      /* MODE_MOUSEX10: no button release reporting */
+      if (IS_SET(MODE_MOUSEX10))
+        return;
+      if (button == 64 || button == 65)
+        return;
+    }
+  }
+
+  if (!IS_SET(MODE_MOUSEX10)) {
+    button += ((state & ShiftMask) ? 4 : 0) + ((state & Mod4Mask) ? 8 : 0) +
+              ((state & ControlMask) ? 16 : 0);
+  }
+
+  if (IS_SET(MODE_MOUSESGR)) {
+    len = snprintf(buf, sizeof(buf), "\033[<%d;%d;%d%c", button, x + 1, y + 1,
+                   e->xbutton.type == ButtonRelease ? 'm' : 'M');
+  } else if (x < 223 && y < 223) {
+    len = snprintf(buf, sizeof(buf), "\033[M%c%c%c", 32 + button, 32 + x + 1,
+                   32 + y + 1);
+  } else {
+    return;
+  }
+
+  ttywrite(buf, len, 0);
+}
+
+uint buttonmask(uint button) {
+  return button == Button1   ? Button1Mask
+         : button == Button2 ? Button2Mask
+         : button == Button3 ? Button3Mask
+         : button == Button4 ? Button4Mask
+         : button == Button5 ? Button5Mask
+                             : 0;
+}
+
+int mouseaction(XEvent *e, uint release) {
+  MouseShortcut *ms;
+
+  /* ignore Button<N>mask for Button<N> - it's set on release */
+  uint state = e->xbutton.state & ~buttonmask(e->xbutton.button);
+
+  for (ms = mshortcuts; ms < mshortcuts + LEN(mshortcuts); ms++) {
+    if (ms->release == release && ms->button == e->xbutton.button &&
+        (match(ms->mod, state) || /* exact or forced */
+         match(ms->mod, state & ~forcemousemod))) {
+      ms->func(&(ms->arg));
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+void bpress(XEvent *e) {
+  struct timespec now;
+  int snap;
+
+  if (IS_SET(MODE_MOUSE) && !(e->xbutton.state & forcemousemod)) {
+    mousereport(e);
+    return;
+  }
+
+  if (mouseaction(e, 0))
+    return;
+
+  if (e->xbutton.button == Button1) {
+    /*
+     * If the user clicks below predefined timeouts specific
+     * snapping behaviour is exposed.
+     */
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    if (TIMEDIFF(now, xsel.tclick2) <= tripleclicktimeout) {
+      snap = SNAP_LINE;
+    } else if (TIMEDIFF(now, xsel.tclick1) <= doubleclicktimeout) {
+      snap = SNAP_WORD;
+    } else {
+      snap = 0;
+    }
+    xsel.tclick2 = xsel.tclick1;
+    xsel.tclick1 = now;
+
+    selstart(evcol(e), evrow(e), snap);
+  }
+}
+
+void propnotify(XEvent *e) {
+  XPropertyEvent *xpev;
+  Atom clipboard = XInternAtom(xw.dpy, "CLIPBOARD", 0);
+
+  xpev = &e->xproperty;
+  if (xpev->state == PropertyNewValue &&
+      (xpev->atom == XA_PRIMARY || xpev->atom == clipboard)) {
+    selnotify(e);
+  }
+}
+
+void selnotify(XEvent *e) {
+  ulong nitems, ofs, rem;
+  int format;
+  uchar *data, *last, *repl;
+  Atom type, incratom, property = None;
+
+  incratom = XInternAtom(xw.dpy, "INCR", 0);
+
+  ofs = 0;
+  if (e->type == SelectionNotify)
+    property = e->xselection.property;
+  else if (e->type == PropertyNotify)
+    property = e->xproperty.atom;
+
+  if (property == None)
+    return;
+
+  do {
+    if (XGetWindowProperty(xw.dpy, xw.win, property, ofs, BUFSIZ / 4, False,
+                           AnyPropertyType, &type, &format, &nitems, &rem,
+                           &data)) {
+      fprintf(stderr, "Clipboard allocation failed\n");
+      return;
+    }
+
+    if (e->type == PropertyNotify && nitems == 0 && rem == 0) {
+      /*
+       * If there is some PropertyNotify with no data, then
+       * this is the signal of the selection owner that all
+       * data has been transferred. We won't need to receive
+       * PropertyNotify events anymore.
+       */
+      MODBIT(xw.attrs.event_mask, 0, PropertyChangeMask);
+      XChangeWindowAttributes(xw.dpy, xw.win, CWEventMask, &xw.attrs);
+    }
+
+    if (type == incratom) {
+      /*
+       * Activate the PropertyNotify events so we receive
+       * when the selection owner does send us the next
+       * chunk of data.
+       */
+      MODBIT(xw.attrs.event_mask, 1, PropertyChangeMask);
+      XChangeWindowAttributes(xw.dpy, xw.win, CWEventMask, &xw.attrs);
+
+      /*
+       * Deleting the property is the transfer start signal.
+       */
+      XDeleteProperty(xw.dpy, xw.win, (int)property);
+      continue;
+    }
+
+    /*
+     * As seen in getsel:
+     * Line endings are inconsistent in the terminal and GUI world
+     * copy and pasting. When receiving some selection data,
+     * replace all '\n' with '\r'.
+     * FIXME: Fix the computer world.
+     */
+    repl = data;
+    last = data + nitems * format / 8;
+    while ((repl = memchr(repl, '\n', last - repl))) {
+      *repl++ = '\r';
+    }
+
+    if (IS_SET(MODE_BRCKTPASTE) && ofs == 0)
+      ttywrite("\033[200~", 6, 0);
+    ttywrite((char *)data, nitems * format / 8, 1);
+    if (IS_SET(MODE_BRCKTPASTE) && rem == 0)
+      ttywrite("\033[201~", 6, 0);
+    XFree(data);
+    /* number of 32-bit chunks returned */
+    ofs += nitems * format / 32;
+  } while (rem > 0);
+
+  /*
+   * Deleting the property again tells the selection owner to send the
+   * next data chunk in the property.
+   */
+  XDeleteProperty(xw.dpy, xw.win, (int)property);
+}
+
+void xclipcopy(void) { clipcopy(NULL); }
+
+void selclear_(XEvent *e) { selclear(); }
+
+void selrequest(XEvent *e) {
+  XSelectionRequestEvent *xsre;
+  XSelectionEvent xev;
+  Atom xa_targets, string, clipboard;
+  char *seltext;
+
+  xsre = (XSelectionRequestEvent *)e;
+  xev.type = SelectionNotify;
+  xev.requestor = xsre->requestor;
+  xev.selection = xsre->selection;
+  xev.target = xsre->target;
+  xev.time = xsre->time;
+  if (xsre->property == None)
+    xsre->property = xsre->target;
+
+  /* reject */
+  xev.property = None;
+
+  xa_targets = XInternAtom(xw.dpy, "TARGETS", 0);
+  if (xsre->target == xa_targets) {
+    /* respond with the supported type */
+    string = xsel.xtarget;
+    XChangeProperty(xsre->display, xsre->requestor, xsre->property, XA_ATOM, 32,
+                    PropModeReplace, (uchar *)&string, 1);
+    xev.property = xsre->property;
+  } else if (xsre->target == xsel.xtarget || xsre->target == XA_STRING) {
+    /*
+     * xith XA_STRING non ascii characters may be incorrect in the
+     * requestor. It is not our problem, use utf8.
+     */
+    clipboard = XInternAtom(xw.dpy, "CLIPBOARD", 0);
+    if (xsre->selection == XA_PRIMARY) {
+      seltext = xsel.primary;
+    } else if (xsre->selection == clipboard) {
+      seltext = xsel.clipboard;
+    } else {
+      fprintf(stderr, "Unhandled clipboard selection 0x%lx\n", xsre->selection);
+      return;
+    }
+    if (seltext != NULL) {
+      XChangeProperty(xsre->display, xsre->requestor, xsre->property,
+                      xsre->target, 8, PropModeReplace, (uchar *)seltext,
+                      strlen(seltext));
+      xev.property = xsre->property;
+    }
+  }
+
+  /* all done, send a notification to the listener */
+  if (!XSendEvent(xsre->display, xsre->requestor, 1, 0, (XEvent *)&xev))
+    fprintf(stderr, "Error sending SelectionNotify event\n");
+}
+
+void setsel(char *str, Time t) {
+  if (!str)
+    return;
+
+  free(xsel.primary);
+  xsel.primary = str;
+
+  XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
+  if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
+    selclear();
+  clipcopy(NULL);
+}
+
+void xsetsel(char *str) { setsel(str, CurrentTime); }
+
+void brelease(XEvent *e) {
+  if (IS_SET(MODE_MOUSE) && !(e->xbutton.state & forcemousemod)) {
+    mousereport(e);
+    return;
+  }
+
+  if (mouseaction(e, 1))
+    return;
+  if (e->xbutton.button == Button1)
+    mousesel(e, 1);
+}
+
+void bmotion(XEvent *e) {
+  if (IS_SET(MODE_MOUSE) && !(e->xbutton.state & forcemousemod)) {
+    mousereport(e);
+    return;
+  }
+
+  mousesel(e, 0);
+}
+
+void cresize(int width, int height) {
+  int col, row;
+
+  if (width != 0)
+    win.w = width;
+  if (height != 0)
+    win.h = height;
+
+  col = (win.w - 2 * borderpx) / win.cw;
+  row = (win.h - 2 * borderpx) / win.ch;
+  col = MAX(1, col);
+  row = MAX(1, row);
+
+  tresize(col, row);
+  xresize(col, row);
+  ttyresize(win.tw, win.th);
+}
+
+void xresize(int col, int row) {
+  win.tw = col * win.cw;
+  win.th = row * win.ch;
+
+  XFreePixmap(xw.dpy, xw.buf);
+  xw.buf =
+      XCreatePixmap(xw.dpy, xw.win, win.w, win.h, DefaultDepth(xw.dpy, xw.scr));
+  XftDrawChange(xw.draw, xw.buf);
+  xclear(0, 0, win.w, win.h);
+
+  /* resize to new width */
+  xw.specbuf = xrealloc(xw.specbuf, col * sizeof(GlyphFontSpec));
+}
+
+ushort sixd_to_16bit(int x) { return x == 0 ? 0 : 0x3737 + 0x2828 * x; }
+
+int xloadcolor(int i, const char *name, Color *ncolor) {
+  XRenderColor color = {.alpha = 0xffff};
+
+  if (loaded) {
+    for (cp = dc.col; cp < &dc.col[dc.collen]; ++cp)
+      XftColorFree(xw.dpy, xw.vis, xw.cmap, cp);
+  } else {
+    dc.collen = MAX(LEN(colorname), 256);
+    dc.col = xmalloc(dc.collen * sizeof(Color));
+  }
+
+  for (i = 0; i < dc.collen; i++)
+    if (!xloadcolor(i, NULL, &dc.col[i])) {
+      if (colorname[i])
+        die("could not allocate color '%s'\n", colorname[i]);
+      else
+        die("could not allocate color %d\n", i);
+    }
+  loaded = 1;
+}
+
 int xsetcolorname(int x, const char *name) {
   Color ncolor;
 
