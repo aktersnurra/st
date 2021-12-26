ceca10754a40c388d6e783224b009a03d5c833e3
Raheman Vaiya <r.vaiya@gmail.com>
2021-12-26 18:57:04 +0100

    Merge 8e310303903792c010d03c046ba75f8b18f7d3a7

--- st.h
+++ st.h
@@ -116,6 +116,7 @@ size_t utf8encode(Rune, char *);
 void *xmalloc(size_t);
 void *xrealloc(void *, size_t);
 char *xstrdup(const char *);
+int xgetcolor(int x, unsigned char *r, unsigned char *g, unsigned char *b);
 
 int isboxdraw(Rune);
 ushort boxdrawindex(const Glyph *);
@@ -137,4 +138,5 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern unsigned int defaultcs;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
