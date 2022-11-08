96c38592a2df2887cb6c173710e450a8ea1f6910
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-11-08 23:43:11 +0100

    Remove joy

--- config.h
+++ config.h
@@ -13,8 +13,7 @@
 static char *font = "monospace:pixelsize=11:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    "Symbols Nerd Font:pixelsize=12:antialias=true:autohint=true",
-    "JoyPixels:pixelsize=10:antialias=true:autohint=true"};
+    "Symbols Nerd Font:pixelsize=12:antialias=true:autohint=true"};
 
 static int borderpx = 2;
 /*
